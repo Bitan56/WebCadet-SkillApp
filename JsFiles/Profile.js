a3daf3f4f40899a038fb33bcf190ff432e91030f
Bitan56 <bitanchakraborty90@gmail.com>
2026-07-16 18:59:31 +0530

    add

--- JsFiles/Profile.js
+++ JsFiles/Profile.js
@@ -7,13 +7,27 @@ const viewSkills = document.querySelector('.view-skills')
 const sidebarViewSkills = document.querySelector('.sidebar ul .view-skills')
 const uploadSkills = document.querySelector('.upload-skills')
 const sidebarUploadSkills = document.querySelector('.sidebar ul .upload-skills')
+const homeBtn = document.querySelector('.home-btn')
+const sidebarHomeBtn = document.querySelector('.sidebar ul .home-btn')
+const dataContainer = document.querySelector('.data-container')
 
-//Making the home button work
-dynBtn.addEventListener('click',()=>{
-    window.location.href = '../index.html'
+async function checkAuth() {
+    const isAuth = await localStorage.getItem('isLogged')
+    if(!isAuth){
+        window.location.href = './SignIn.html'
+    }
+}
+
+checkAuth()
+
+//Making the logout button work
+dynBtn.addEventListener('click', async () => {
+    await localStorage.clear()
+    location.reload()
 })
-sidebarDynBtn.addEventListener('click',()=>{
-    window.location.href = '../index.html'
+sidebarDynBtn.addEventListener('click', async () => {
+    await localStorage.clear()
+    location.reload()
 })
 
 //Writing GSAP timelines
@@ -38,16 +52,44 @@ crossBtn.addEventListener('click', () => {
 
 //Uploading skills button
 uploadSkills.addEventListener('click', () => {
-        window.location.href = './UploadSkills.html'
+    window.location.href = './UploadSkills.html'
 })
 sidebarUploadSkills.addEventListener('click', () => {
-        window.location.href = './UploadSkills.html'
+    window.location.href = './UploadSkills.html'
 })
 
 //Activating the View Skills button
-viewSkills.addEventListener('click',()=>{
+viewSkills.addEventListener('click', () => {
     window.location.href = './ViewSkills.html'
 })
-sidebarViewSkills.addEventListener('click',()=>{
+sidebarViewSkills.addEventListener('click', () => {
     window.location.href = './ViewSkills.html'
 })
+
+//Activating the Home button
+homeBtn.addEventListener('click',()=>{
+    window.location.href = '../index.html'
+})
+sidebarDynBtn.addEventListener('click',()=>{
+    window.location.href = '../index.html'
+})
+
+//Injecting the Profile Data
+dataContainer.innerHTML = `
+<div class="data-box">
+            <div class="data-head">Name:</div>
+            <div class="data-content">${localStorage.getItem('name')}</div>
+        </div>
+        <div class="data-box">
+            <div class="data-head">UserName:</div>
+            <div class="data-content">@${localStorage.getItem('username')}</div>
+        </div>
+        <div class="data-box">
+            <div class="data-head">Department:</div>
+            <div class="data-content">${localStorage.getItem('department')}</div>
+        </div>
+        <div class="data-box">
+            <div class="data-head">No. of Skills:</div>
+            <div class="data-content">-/-</div>
+        </div>
+`
